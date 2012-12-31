e928c5c7f96f10a4116855582577a27cd80e877f
yawkat <me@yawk.at>
2012-12-31 15:00:25 +0100

    finished MetaSearchIterable

--- src/java/main/at/yawk/fimfiction/api/parsers/MetaSearchIterable.java
+++ src/java/main/at/yawk/fimfiction/api/parsers/MetaSearchIterable.java
@@ -3,12 +3,10 @@ package at.yawk.fimfiction.api.parsers;
 import java.util.Iterator;
 
 import at.yawk.fimfiction.api.InternetAccess;
-import at.yawk.fimfiction.api.SearchOrder;
 import at.yawk.fimfiction.api.SearchRequest;
-import at.yawk.fimfiction.api.SimpleInternetAccess;
 import at.yawk.fimfiction.api.StoryAccess;
 import at.yawk.fimfiction.api.VisibleStoryMeta;
-import at.yawk.fimfiction.api.factories.SearchRequestFactory;
+import at.yawk.fimfiction.api.util.SearchUtil;
 
 public class MetaSearchIterable extends SearchIterable<StoryAccess<VisibleStoryMeta>> {
 	public MetaSearchIterable(SearchRequest request, InternetAccess internet) {
@@ -17,16 +15,6 @@ public class MetaSearchIterable extends SearchIterable<StoryAccess<VisibleStoryM
 
 	@Override
 	public Iterator<StoryAccess<VisibleStoryMeta>> iterator() {
-		return new MetaSearchIterator(getSearchTerm(), getInternet());
-	}
-
-	/**
-	 * Testing.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		for(StoryAccess<VisibleStoryMeta> s : new MetaSearchIterable(new SearchRequestFactory().setSearchOrder(SearchOrder.RATING), new SimpleInternetAccess())) {
-			System.out.println(s.getMeta().getTitle());
-		}
+		return new MetaSearchIterator(SearchUtil.getSearchGet(getSearchRequest()), getInternet());
 	}
 }
