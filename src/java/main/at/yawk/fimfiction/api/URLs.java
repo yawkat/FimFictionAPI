44c3af9d21129d710e75c6128faf748d5e8dd274
yawkat <me@yawk.at>
2013-01-09 22:30:11 +0100

    Corrected indentation for URLs, added RSS ID favorite unread search thing

--- src/java/main/at/yawk/fimfiction/api/URLs.java
+++ src/java/main/at/yawk/fimfiction/api/URLs.java
@@ -6,22 +6,24 @@ package at.yawk.fimfiction.api;
  * @author Yawkat
  */
 public class URLs {
-	public static final String	FIMFICTION			= "http://www.fimfiction.net/";
-	public static final String	FIMFICTION_STATIC	= "http://www.fimfiction-static.net/";
-	public static final String	FIMFICTION_AJAX		= FIMFICTION + "ajax/";
-	public static final String	FIMFICTION_DOWNLOAD	= FIMFICTION + "download_";
-	public static final String	SEARCH_BASE			= FIMFICTION + "index.php?";
-	public static final String	API_BASE			= FIMFICTION + "api/story.php?story=";
-	public static final String	LOGIN				= FIMFICTION_AJAX + "login.php";
-	public static final String	READLATER			= FIMFICTION_AJAX + "add_read_it_later.php";
-	public static final String	FAVORITE			= FIMFICTION_AJAX + "add_favourite.php";
-	public static final String	UNREAD				= FIMFICTION_AJAX + "toggle_read.php";
-	public static final String	RATE				= FIMFICTION_AJAX + "rate.php";
-	public static final String	STORY				= FIMFICTION + "story/";
-	public static final String	DOWNLOAD_TXT		= FIMFICTION_DOWNLOAD + "story.php?story=";
-	public static final String	DOWNLOAD_HTML		= FIMFICTION_DOWNLOAD + "story.php?html&story=";
-	public static final String	DOWNLOAD_EPUB		= FIMFICTION_DOWNLOAD + "epub.php?story=";
-	public static final String	DOWNLOAD_CHAPTER_TXT= FIMFICTION_DOWNLOAD + "chapter.php?chapter=";
-	public static final String	DOWNLOAD_CHAPTER_HTML= FIMFICTION_DOWNLOAD + "chapter.php?html&chapter=";
+	public static final String	FIMFICTION				= "http://www.fimfiction.net/";
+	public static final String	FIMFICTION_STATIC		= "http://www.fimfiction-static.net/";
+	public static final String	FIMFICTION_AJAX			= FIMFICTION + "ajax/";
+	public static final String	FIMFICTION_DOWNLOAD		= FIMFICTION + "download_";
+	public static final String	SEARCH_BASE				= FIMFICTION + "index.php?";
+	public static final String	API_BASE				= FIMFICTION + "api/story.php?story=";
+	public static final String	LOGIN					= FIMFICTION_AJAX + "login.php";
+	public static final String	READLATER				= FIMFICTION_AJAX + "add_read_it_later.php";
+	public static final String	FAVORITE				= FIMFICTION_AJAX + "add_favourite.php";
+	public static final String	UNREAD					= FIMFICTION_AJAX + "toggle_read.php";
+	public static final String	RATE					= FIMFICTION_AJAX + "rate.php";
+	public static final String	STORY					= FIMFICTION + "story/";
+	public static final String	DOWNLOAD_TXT			= FIMFICTION_DOWNLOAD + "story.php?story=";
+	public static final String	DOWNLOAD_HTML			= FIMFICTION_DOWNLOAD + "story.php?html&story=";
+	public static final String	DOWNLOAD_EPUB			= FIMFICTION_DOWNLOAD + "epub.php?story=";
+	public static final String	DOWNLOAD_CHAPTER_TXT	= FIMFICTION_DOWNLOAD + "chapter.php?chapter=";
+	public static final String	DOWNLOAD_CHAPTER_HTML	= FIMFICTION_DOWNLOAD + "chapter.php?html&chapter=";
+	public static final String	RSS_BASE				= FIMFICTION + "rss/";
+	public static final String	RSS_FAVORITE			= RSS_BASE + "tracking.php?user=";
 	
 }
---
+++ src/java/main/at/yawk/fimfiction/api/parsers/RSSIdentifierFavoriteIterable.java
@@ -0,0 +1,55 @@
+package at.yawk.fimfiction.api.parsers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import at.yawk.fimfiction.api.Identifier;
+import at.yawk.fimfiction.api.InternetAccess;
+import at.yawk.fimfiction.api.SimpleInternetAccess;
+import at.yawk.fimfiction.api.URLs;
+import at.yawk.fimfiction.api.immutable.SimpleIdentifier;
+
+public class RSSIdentifierFavoriteIterable implements Iterable<Identifier> {
+	private final String			url;
+	private final InternetAccess	internet;
+	
+	public RSSIdentifierFavoriteIterable(Identifier userId, InternetAccess internet) {
+		this.url = URLs.RSS_FAVORITE + userId.getId();
+		this.internet = internet;
+	}
+
+	@Override
+	public Iterator<Identifier> iterator() {
+		try {
+			final XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(internet.connect(new URL(url)).getInputStream());
+			final List<Identifier> elements = new ArrayList<Identifier>();
+			while(reader.hasNext()) {
+				final int event = reader.next();
+				if(event == XMLStreamReader.START_ELEMENT && reader.getName().getLocalPart().equals("link")) {
+					if(reader.next() == XMLStreamReader.CHARACTERS) {
+						final String s = reader.getText().trim().substring(32);
+						elements.add(new SimpleIdentifier(Integer.parseInt(s.substring(0, s.indexOf('/')))));
+					}
+				}
+			}
+			return elements.iterator();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} catch(XMLStreamException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		for(Identifier i : new RSSIdentifierFavoriteIterable(new SimpleIdentifier(30603), new SimpleInternetAccess()))
+			System.out.println(i.getId());
+	}
+}
